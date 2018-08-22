73d87a3722d443940030b4d81285c355f671c38e
WhippaSnappers <24751261+WhippaSnappers@users.noreply.github.com>
2018-08-22 18:29:27 +1000

    Create russian.lua

---
+++ blogs/russian.lua
@@ -0,0 +1,156 @@
+--[[
+
+	bLogs Russian
+	
+	Contributors:
+	
+	WhippaSnappers - STEAM_1:1:82756840
+
+]]
+
+bLogs.Language = {
+
+	-- First column in the logs, shows the date/time of the log
+	when = "Когда",
+
+	permission_failure = "У вас нет прав для выполнения этого действия.",
+
+	-- When the user is an admin, this will be in the window title.
+	admin_mode = "Режим Администратора",
+
+	-- The button to view all logs stored.
+	all_logs = "Все Логи",
+
+	-- The button to view all players stored in bLogs.
+	all_players = "Все Игроки",
+
+	-- The tab at the top to view players stored in bLogs.
+	players = "Игроки",
+
+	-- The message shown to the player when they first open bLogs.
+	getting_data = "Получаю информацию, пожалуйста, подождите...",
+
+	-- The message shown when there's no logs to show.
+	no_data = “Нет логов!",
+
+	advanced_search = “Расширенный Поиск",
+	quick_search = “Быстрый Поиск",
+	search = “Поиск",
+
+	-- The button to open the SteamID Finder (steamid.venner.io)
+	steamid_finder = “Поиск SteamID",
+
+	-- The message in the players panel instructing the user to select something
+	-- to view players.
+	select_something = “Выберите что-нибудь“,
+
+	-- The button at the pagination that allows you to jump to a specific page.
+	jump = "Перейти",
+
+	-- The title of the window that pops up when you click "Jump"
+	jump_to_page = “Перейти на страницу...",
+
+	-- The text in that window instructing the user.
+	-- %d is the number of pages that exist
+	type_page = “Введите номер страницы (всего - %d страниц)",
+
+	cancel = "Отмена",
+
+	error_over_pagenum = “Такой страницы не существует",
+	error_not_num = “Это был не номер",
+
+	error = "Ошибка",
+
+	-- Left click to copy [player] name
+	involved_tooltip = “Правый клик - опции“,
+
+	-- View logs that that this player is involved in
+	view_player_logs = “Просмотреть Логи Игрока",
+
+	-- Copy [something] [to clipboard]
+	copy = "Скопировать %s",
+
+	profile = “Профиль Steam",
+
+	left_click_to_copy = “Левый клик - скопировать“,
+
+	modules = "Модули",
+
+	custom = “Кастомный”,
+
+	enter_a_steamid = "Введите SteamID...",
+
+	error_not_steamid = “Это был не SteamID.",
+
+	-- Used in the advanced search box
+	-- "Does the log contain this text?"
+	contains = “Еще содержит...",
+
+	-- "Colour Mode" is whether or not the logs are shown with coloured text.
+	colour_mode = “Цветной Режим",
+
+	jump_to_date = “Перейти к Дате",
+
+	jump_to_date_tooltip = “Правый клик - перейти к дате/времени",
+
+	type_date = “Введите дату в формате: DD/MM/YYYY (%s/%s/%s)",
+
+	error_not_date = “Дата введена неправильно. Она должна быть в формате: DD/MM/YYYY (%s/%s/%s)",
+
+	type_time = “Введите временную отметку. Она должна быть в формате: HH:MM (%s:%s) (24 часа)\nНе вводите ничего, чтобы не учитывать время.”,
+
+	error_not_time = “Временная отметка введена неправильно. Она должна быть в формате: HH:MM (%s:%s) (24 часа)",
+
+	archive = "Архив",
+
+	searching = "Поиск",
+
+	search_archive = “Поиск в Архиве",
+
+	search_archive_warning = “Поиск по архиву может привести к лагам на сервере при использовании локальной базы данных. Используйте это только, если действительно нужно.",
+
+	logging = “Запись в лог",
+
+	save = "Сохранить",
+
+	enabled = “Включено",
+
+	disabled = "Отключено",
+
+	disable = "Отключить",
+
+	enable = "Включить",
+
+	give = "Дать",
+
+	take = "Забрать",
+
+	new_steamid = "Новый SteamID",
+
+	new_usergroup = “Новая Группа",
+
+	usergroup_name = “Название Группы",
+
+	-- "Delete this entry from permissions"
+	delete_entry = “Удалить",
+
+	usergroup_exists = “Эта группа уже существует! Возможно, существует похожая группа? В таком случае, используйте ее.”,
+
+	steamid_exists = "Этот SteamID уже существует.",
+
+	loading = "Загрузка...",
+
+	support = "Поддержка",
+
+	-- "Player Format" is a setting that contains everything that will be included
+	-- in a log that references a player. For example, it will show the player's job,
+	-- health, armour, etc. depending on the settings.
+	player_format = “Все об Игроке",
+
+	permissions = “Полномочия",
+
+	general_settings = “Основные Настройки",
+
+	operations = "Операции",
+
+}
